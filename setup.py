a6b33405f99f5cbc02399603c1e684a62d645eac
sneha931 <cs22157@velammalitech.edu.in>
2025-10-15 20:53:12 +0530

    setupfile

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="medical chatbot",
+    version="0.1.0",
+    author="sneha",
+    author_email="12042005sneha@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
